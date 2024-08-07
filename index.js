4ed0f173cc5caee630c10e6c4fff1e28bf6f97bb
nothoru <yuiishi022@gmail.com>
2024-08-07 14:26:34 +0800

    Add game logic, change user input from prompt to button
    Careful when targeting ID make sure its place
    in the right element just like the button
    and nested img

--- index.js
+++ index.js
@@ -1,10 +1,66 @@
 const rps = ["rock", "paper", "scissor"];
+let playerScore = 0;
+let computerScore = 0;
+const para = document.querySelector(".para");
+const cpuDrew = document.querySelector(".cpuDraws");
+const userDraws = document.querySelector(".userDraws");
 
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-  let user = prompt("Enter rock, paper, scissor");
-  return user.toLowerCase();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "paper" && computerChoice == "rock") {
+    para.textContent = "You won! Paper beats Rock";
+    playerScore += 1;
+  } else if (humanChoice == "rock" && computerChoice == "scissor") {
+    para.textContent = "You won! Rock beats Scissor";
+    playerScore += 1;
+  } else if (humanChoice == "scissor" && computerChoice == "paper") {
+    para.textContent = "You won! Scissor beats Paper";
+    playerScore += 1;
+  } else if (humanChoice == computerChoice) {
+    para.textContent = `Its a tie`;
+  } else {
+    para.textContent = "You lose! " + computerChoice + " beats " + humanChoice;
+    computerScore += 1;
+  }
+
+  cpuDrew.textContent = `CPU: ${computerChoice}`;
+  userDraws.textContent = `USER: ${humanChoice}`;
 }
+
+// function playGame() {
+//   let gameOn = true;
+//   do {
+//     let cpu = getComputerChoice();
+//     playRound(user, cpu);
+
+//     if (playerScore == 5 || computerScore == 5) {
+//       gameOn = false;
+//     }
+//   } while (gameOn);
+//
+
+let playerOption = document.querySelector("#playerOption");
+
+playerOption.addEventListener("click", (event) => {
+  let target = event.target;
+  let user;
+  switch (target.id) {
+    case "rock":
+      user = rps[0];
+      break;
+    case "paper":
+      user = rps[1];
+      break;
+    case "scissor":
+      user = rps[2];
+      break;
+  }
+
+  playRound(user, getComputerChoice());
+});
+
+console.log(`USER: ${playerScore}`);
+console.log(`CPU: ${computerScore}`);
