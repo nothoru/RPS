83d6316c4bca2e79f711b2eb9fda7deee56020d5
nothoru <yuiishi022@gmail.com>
2024-08-07 14:25:08 +0800

    Added game logic, changed from prompt to button
    Using eventListener for the buttons be careful
    when you put the id in the button instead in the
    nested image if its in the button it will not take
    the ID which will return undefined

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const rps = ["rock", "paper", "scissor"];
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * 3)];
 }
+
+function getHumanChoice() {
+  let user = prompt("Enter rock, paper, scissor");
+  return user.toLowerCase();
+}
