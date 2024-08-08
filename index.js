b7a722ba1f365114649809129fed091d90e4dc31
nothoru <yuiishi022@gmail.com>
2024-08-08 16:32:47 +0800

    Added styles/Issues
    Added:
    Video shows when USER picks
    
    Issues:
    Video shows what CPU drew early text content
    is 3 seconds delay

--- index.js
+++ index.js
@@ -5,28 +5,42 @@ const para = document.querySelector(".para");
 const cpuDrew = document.querySelector(".cpuDraws");
 const userDraws = document.querySelector(".userDraws");
 
+const videoPaths = {
+  rock: "vid/rock.mp4",
+  paper: "vid/paper.mp4",
+  scissor: "vid/scissor.mp4",
+};
+
 function getComputerChoice() {
   return rps[Math.floor(Math.random() * 3)];
 }
 
 function playRound(humanChoice, computerChoice) {
-  if (humanChoice == "paper" && computerChoice == "rock") {
-    para.textContent = "You won! Paper beats Rock";
-    playerScore += 1;
-  } else if (humanChoice == "rock" && computerChoice == "scissor") {
-    para.textContent = "You won! Rock beats Scissor";
-    playerScore += 1;
-  } else if (humanChoice == "scissor" && computerChoice == "paper") {
-    para.textContent = "You won! Scissor beats Paper";
-    playerScore += 1;
-  } else if (humanChoice == computerChoice) {
-    para.textContent = `Its a tie`;
-  } else {
-    para.textContent = "You lose! " + computerChoice + " beats " + humanChoice;
-    computerScore += 1;
-  }
+  const videoPlayer = document.querySelector("#videoPlayer");
 
-  cpuDrew.textContent = `CPU: ${computerChoice}`;
+  videoPlayer.src = videoPaths[computerChoice];
+  videoPlayer.style.display = "block";
+
+  videoPlayer.addEventListener("ended", () => {
+    if (humanChoice == "paper" && computerChoice == "rock") {
+      para.textContent = "You won! Paper beats Rock";
+      playerScore += 1;
+    } else if (humanChoice == "rock" && computerChoice == "scissor") {
+      para.textContent = "You won! Rock beats Scissor";
+      playerScore += 1;
+    } else if (humanChoice == "scissor" && computerChoice == "paper") {
+      para.textContent = "You won! Scissor beats Paper";
+      playerScore += 1;
+    } else if (humanChoice == computerChoice) {
+      para.textContent = `Its a tie`;
+    } else {
+      para.textContent =
+        "You lose! " + computerChoice + " beats " + humanChoice;
+      computerScore += 1;
+    }
+    cpuDrew.textContent = `CPU: ${computerChoice}`;
+  });
+
   userDraws.textContent = `USER: ${humanChoice}`;
 }
 
